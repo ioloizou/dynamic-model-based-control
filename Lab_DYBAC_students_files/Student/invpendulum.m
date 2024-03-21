21ab69666db3def7dd17e1de017f637cd7b807d9
Ioannis Loizou <yiannisloizou@gmail.com>
2024-03-21 11:38:49 +0100

    finished teh observer

--- Lab_DYBAC_students_files/Student/invpendulum.m
+++ Lab_DYBAC_students_files/Student/invpendulum.m
@@ -36,14 +36,25 @@ F = -acker(A, B, poles_desired);
 
 %% Plotting
 
-x=out.x;
-tout=out.tout;
-u=out.u;
+% x=out.x;
+% tout=out.tout;
+% u=out.u;
+% 
+% f1=figure(1) ;
+% set(f1,'position',[1 305 672 500])
+% subplot(321),plot(tout,x( :,1)),title('angle'),grid,
+% subplot(323),plot(tout,x( :,2)),title('derivative of angle'),grid
+% subplot(322),plot(tout,x( :,3)),title('position'),grid,
+% subplot(324),plot(tout,x( :,4)),title('derivative of position'),grid
+% subplot(325),plot(tout,u),title('control'),grid
 
-f1=figure(1) ;
-set(f1,'position',[1 305 672 500])
-subplot(321),plot(tout,x( :,1)),title('angle'),grid,
-subplot(323),plot(tout,x( :,2)),title('derivative of angle'),grid
-subplot(322),plot(tout,x( :,3)),title('position'),grid,
-subplot(324),plot(tout,x( :,4)),title('derivative of position'),grid
-subplot(325),plot(tout,u),title('control'),grid
+%% Observer analysis
+C=[1 0 0 0; 0 0 1 0;]; % C of the observer
+obs=obsv(A,C);
+obs_rank=rank(obs);
+
+%% Observer design
+
+poles_fast = [-100+1i, -100-1i, -200-2*1i, -200+2*1i];
+K_fast=place(A.',C.',poles_fast).';
+K=place(A.',C.',poles_desired).';
